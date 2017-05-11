b991af3936951fe8a5a453afd9a71d6892798ac8
lichao <lichao2012@gmail.com>
2017-05-11 16:38:44 +0800

    init

---
+++ jquery.progressbar.js
@@ -0,0 +1,35 @@
+(function( $ ) {
+  $.fn.progressbar = function ( options ) {
+
+    var opts = $.extend({ }, options);
+
+    return this.each(function() {
+      var $this = $(this);
+
+      var $ul = $('<ul>').attr('class', 'progressbar');
+
+      var currentIdx = -1
+
+      $.each(opts.steps, function(index, value) {
+        var $li = $('<li>').text(value.replace('@', '').replace('~', ''));
+        $li.css('width', (100 / opts.steps.length) + '%');
+        if(value.indexOf('@') > -1) {
+          $li.addClass('current');
+          currentIdx = index;
+        }
+
+        if(value.indexOf('~') > -1) {
+          $li.addClass('fail');
+        }
+
+        $ul.append($li);
+      });
+
+      for(var i = 0; i < currentIdx; i++) {
+        $($ul.find('li')[i]).addClass('done');
+      }
+
+      $this.append($ul);
+    });
+  };
+})( jQuery );
